cf004b451b8a6bbf8b5ba143cd20650f8ae03361
MohamadMahdy Raeisi <66519118+RedDeadGhost@users.noreply.github.com>
2023-02-01 00:32:49 +0330

    Back-end files uploaded
    Back-end files uploaded to improve the adding users api.

--- BuyProducts_BackEnd_Files/BPBE_App/serializers.py
+++ BuyProducts_BackEnd_Files/BPBE_App/serializers.py
@@ -12,3 +12,4 @@ class ShopUsers_Serializer(serializers.ModelSerializer):
     class Meta:
         model = ShopUsers_Model
         fields = '__all__'
+        read_only_fields = ('id',)
--- BuyProducts_BackEnd_Files/BPBE_App/views.py
+++ BuyProducts_BackEnd_Files/BPBE_App/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.core.cache import cache
 from rest_framework.response import Response
-from rest_framework import status
+# from rest_framework.permissions import BasePermission
+from rest_framework import status, viewsets, generics
 from datetime import datetime
 from django.views.generic import TemplateView, DetailView, ListView, CreateView, UpdateView
 from django.views.generic.edit import DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from rest_framework import viewsets
 from . import models
 from . import serializers
 from . import forms
@@ -16,19 +16,25 @@ class AllProducts_Viewset(viewsets.ModelViewSet):
     queryset = models.ShopProducts_Model.objects.all()
     serializer_class = serializers.ShopProducts_Serializer
 
+# class AllUsers_Viewset(generics.ListCreateAPIView):
+    # queryset = models.ShopUsers_Model.objects.all()
+    # serializer_class = serializers.ShopUsers_Serializer
 
 # my_obj = AllProducts_Viewset()
 
 class AllUsers_Viewset(viewsets.ModelViewSet):
     queryset = models.ShopUsers_Model.objects.all()
     serializer_class = serializers.ShopUsers_Serializer
-    http_method_names = ['get', 'post', 'put', 'delete']
-    session_id = 'modify-users'
+    # http_method_names = ['get', 'post', 'put', 'delete']
+    # session_id = 'modify-users'
+    # permission_classes = [BasePermission]
 
     def create(self, request):
         users_we_have = []
         for users_all in self.queryset:
             users_we_have.append(users_all.user_usname)
+        # print(users_we_have)
+        print(f"The User {request.data['user_usname']} in the Process ...")
         if request.data['user_usname'] not in users_we_have:
             obj = super().create(request)
             print("---- Create ----")
